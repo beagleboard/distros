f8e86a45318920cdea982fc251f626e5628268d8
Robert Nelson <robertcnelson@gmail.com>
2025-08-08 16:05:44 -0500

    add 2025-08-08 of am335x-debian-13-base
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- format_n_verify.sh
+++ format_n_verify.sh
@@ -18,6 +18,10 @@ run_img () {
 		cat ./device/${device}/debian-13-iot-stable.yml | sed 's/^/  /' >> ./id.yml
 		echo "" >> ./id.yml
 	fi
+	if [ -f ./device/${device}/base-compat.yml ] ; then
+		cat ./device/${device}/base-compat.yml | sed 's/^/  /' >> ./id.yml
+		echo "" >> ./id.yml
+	fi
 	if [ -f ./device/${device}/base-lts.yml ] ; then
 		cat ./device/${device}/base-lts.yml | sed 's/^/  /' >> ./id.yml
 		echo "" >> ./id.yml
