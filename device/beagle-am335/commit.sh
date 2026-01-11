e4f195ab25496a47a80c44e50709c8fa2d229f29
Robert Nelson <robertcnelson@gmail.com>
2026-01-11 16:05:16 -0600

    am335x: 13.3-base/12.13-base 2026-01-11
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- device/beagle-am335/commit.sh
+++ device/beagle-am335/commit.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 . version.sh
-
+cd ../../
+/bin/bash ./format_n_verify.sh
+cd -
 git add ../../bmap-temp/
 git commit -a -m "${device}: ${debian_stable}-base/${debian_old}-base ${date}" -s
