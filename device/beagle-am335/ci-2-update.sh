0b1e12a1247169e59b070598a33ea4259909ed76
Robert Nelson <robertcnelson@gmail.com>
2026-02-12 19:52:42 -0600

    am335x: 13.3-base/12.13-base 2026-02-12
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- device/beagle-am335/ci-2-update.sh
+++ device/beagle-am335/ci-2-update.sh
@@ -7,7 +7,7 @@ server_base_dir="https://rcn-ee.net/rootfs"
 arch="armhf"
 size="4gb"
 
-grab_image () {
+grab_trixie () {
 	server_dir="debian-${arch}-13-base-${kernel_version}"
 	file_prefix="${device}-debian-${debian_stable}-base-${kernel_version}-${arch}-${date}-${size}"
 
@@ -20,7 +20,9 @@ grab_image () {
 
 	wget -c --directory-prefix=/tmp/ ${server_base_dir}/${server_dir}/${date}/${file_prefix}.img.xz.yml.txt
 	mv -v /tmp/${file_prefix}.img.xz.yml.txt ${ymlfile}.yml
+}
 
+grab_bookworm () {
 	server_dir="debian-${arch}-12-base-${kernel_version}"
 	file_prefix="${device}-debian-${debian_old}-base-${kernel_version}-${arch}-${date}-${size}"
 
@@ -35,6 +37,11 @@ grab_image () {
 	mv -v /tmp/${file_prefix}.img.xz.yml.txt old-${ymlfile}.yml
 }
 
+grab_image () {
+	grab_trixie
+	grab_bookworm
+}
+
 kernel_version="v5.10-ti"
 ymlfile="base-compat" ; grab_image
 
@@ -43,5 +50,8 @@ ymlfile="base-lts-612" ; grab_image
 
 kernel_version="v6.18"
 ymlfile="base-lts-618" ; grab_image
+
+kernel_version="v6.19"
+ymlfile="base-stable" ; grab_trixie
 
 #
--- device/beagle-am335/ci-version.sh
+++ device/beagle-am335/ci-version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 device="am335x"
-date="2026-01-22"
+date="2026-02-12"
 debian_stable="13.3"
 debian_old="12.13"
