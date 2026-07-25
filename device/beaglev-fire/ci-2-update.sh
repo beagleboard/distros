24c9d915493e65ad0db4131984183d100aa58bb2
Robert Nelson <robertcnelson@gmail.com>
2026-07-25 17:29:04 -0500

    beaglev-fire: 13-iot 2026-07-25
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- device/beaglev-fire/ci-2-update.sh
+++ device/beaglev-fire/ci-2-update.sh
@@ -7,21 +7,22 @@ server_base_dir="https://rcn-ee.net/rootfs"
 bmap_base_dir="https://gitlab.com/beagle-rootfs/beaglev-fire-ubuntu/-/jobs"
 yml__base_dir="https://beagle-rootfs.gitlab.io/-/beaglev-fire-ubuntu/-/jobs"
 #ci_build
-server_dir="artifacts/raw/deploy"
+bmap_server_dir="artifacts/raw/deploy"
+yml__server_dir="artifacts/deploy"
 
 arch="arm64"
 
 grab_image () {
-	wget -c --directory-prefix=/tmp/ ${bmap_base_dir}/${ci_build}/${server_dir}/${file_prefix}.bmap
+	wget -c --directory-prefix=/tmp/ ${bmap_base_dir}/${ci_build}/${bmap_server_dir}/${file_prefix}.bmap
 	if [ ! -f /tmp/${file_prefix}.bmap ] ; then
-		echo "Failure to get ${bmap_base_dir}/${server_dir}/${file_prefix}.bmap"
+		echo "Failure to get ${bmap_base_dir}/${ci_build}/${bmap_server_dir}/${file_prefix}.bmap"
 		exit 2
 	fi
 	mv -v /tmp/${file_prefix}.bmap ../../bmap-temp/
 
-	wget -c --directory-prefix=/tmp/ ${yml__base_dir}/${server_dir}/${file_prefix}.img.xz.yml.txt
+	wget -c --directory-prefix=/tmp/ ${yml__base_dir}/${ci_build}/${yml__server_dir}/${file_prefix}.img.xz.yml.txt
 	if [ ! -f /tmp/${file_prefix}.img.xz.yml.txt ] ; then
-		echo "Failure to get ${yml__base_dir}/${server_dir}/${file_prefix}.img.xz.yml.txt"
+		echo "Failure to get ${yml__base_dir}/${ci_build}/${yml__server_dir}/${file_prefix}.img.xz.yml.txt"
 		exit 2
 	fi
 	mv -v /tmp/${file_prefix}.img.xz.yml.txt ${ymlfile}.yml
