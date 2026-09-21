d45fb83625ab3a95cca2f60fede5232a77dc020f
Robert Nelson <robertcnelson@gmail.com>
2026-09-21 12:16:22 -0500

    beagleplay: 13.7-iot/13.7-xfce 2026-09-20
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- device/beagle-am62/ci-2-update.sh
+++ device/beagle-am62/ci-2-update.sh
@@ -20,9 +20,17 @@ grab_image () {
 		exit 2
 	fi
 	mv -v /tmp/${file_prefix}.img.xz.yml.txt ${ymlfile}.yml
+	sed -i -e 's:distros/refs/heads/main/sbom-temp:sbom-archive/refs/heads/main/sbom:g' ${ymlfile}.yml
+
+	wget -c --directory-prefix=/tmp/ ${server_base_dir}/${server_dir}/${date}/${file_prefix}.syft.spdx.json.xz
+	if [ ! -f /tmp/${file_prefix}.syft.spdx.json.xz ] ; then
+		echo "Failure to get ${server_base_dir}/${server_dir}/${date}/${file_prefix}.syft.spdx.json.xz"
+		exit 2
+	fi
+	mv -v /tmp/${file_prefix}.syft.spdx.json.xz ../../../sbom-archive/sbom/
 }
 
-size="8gb"
+size="10gb"
 kernel_version="v6.18-k3"
 server_dir="debian-${arch}-13-iot-${kernel_version}"
 file_prefix="${device}-debian-${debian_stable}-iot-${kernel_version}-${arch}-${date}-${size}"
